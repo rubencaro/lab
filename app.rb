6873c89ff8a6b09e11d530cb4e534c471525e475
Josu <josgaez@gmail.com>
2014-02-17 21:40:08 +0100

    suma

--- app.rb
+++ app.rb
@@ -8,3 +8,28 @@ end
 get '/agent' do
   "you're using #{request.user_agent}"
 end
+
+get '/suma/:a/:b' do
+  "#{params[:a].to_i + params[:b].to_i}"
+end
+
+=begin
+
+Desarrollo / deploy
+
+-----  ver info
+git status
+git diff
+git lola
+
+-----  añadir cambios y empaquetarlos
+git add .
+git commit -am 'comentario para el paquete'
+
+-----  enviar a github
+git push github master
+
+-----  enviar a producción
+git push origin master
+
+=end
